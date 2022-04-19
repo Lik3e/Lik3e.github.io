fb15e639aec3f1f4f623110a2c832fbaee45235b
Yurii Nemchninov <anekratia@gmail.com>
2022-04-19 18:10:22 +0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,56 @@
+document.querySelector('#my-name').innerHTML = `${data.first_name} <span class="color-grey">${data.last_name}</span>`;
+document.querySelector('#job-title').textContent = data.job_title;
+document.querySelector('.profile-data').textContent = data.profile;
+/**
+ * section data
+ */
+let expirienceData = '';
+experience.forEach(item => {
+    expirienceData += `<section class="mb-5">`;
+    expirienceData += ` <h4 class="company-name mb-2">${item.company}, <i>${item.from}</i></h4>`;
+    // expirienceData += `<h5>${item.position}</h5>`;
+    expirienceData += `<img src="${item.image}" alt="${item.company}" class="img-thumbnail mt-3 mb-3"`;
+    expirienceData += `</section>`;
+});
+document.querySelector('.experience-list').innerHTML = expirienceData;
+
+/**
+ * contact data
+ */
+
+document.querySelector('.contacts-block').innerHTML += `<li><a href="${data.telegramm}"target='_blank'>Telegramm</a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><a href="mailto:${data.email}">Почта</a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><a href="${data.GitHub}"target='_blank'>GitHub</a></li>`
+// document.querySelector('.contacts-block').innerHTML += `<li><a href="${data.linkedin}">${data.linkedin}</a></li>`
+document.querySelector('.address').innerHTML = `<a href='https://goo.gl/maps/HEXhLRGiKh7ZpH4L7' target='_blank'><p>${data.address}</p></a>`;
+document.querySelector('#my-photo').src = data.photo;
+
+/**
+ * skills
+ */
+
+let skillsData = '';
+data.skills.forEach(item => {
+    skillsData += `<li>${item}</li>`;
+});
+document.querySelector('.skills-list').innerHTML = skillsData;
+
+/**
+ * education
+ */
+
+let educationData = '';
+data.education.forEach(item => {
+    educationData += `<li>${item[0]}, ${item[1]}</li>`;
+});
+document.querySelector('.education-list').innerHTML = educationData;
+
+/**
+ * Achievements
+ */
+
+let achievementsData = '';
+data.achievements.forEach(item => {
+    achievementsData += `<li>${item}</li>`;
+});
+document.querySelector('.achievements-list').innerHTML = achievementsData;
---
+++ js/data.js
@@ -0,0 +1,29 @@
+const data = {
+    "first_name": "Yuriy",
+    "last_name": "Nemchinov",
+    "job_title": "JS Developer",
+    "photo": "images/photo.jpg",
+    "telegramm": "https://t-do.ru/Likeafrost",
+    "email": "anekratia@gmail.com",
+    "GitHub": "https://github.com/L1keaFrost",
+    "linkedin": "link_to_linkedin",
+    "address": "Russia, st.Petersburg",
+    "skills": [
+        "HTML5, CSS",
+        "API, AJAX",
+        "JS functions",
+        "Bootstrap, Adaptive layout",
+        "Promise, ES6",
+        "React JS",
+        "Redux",
+    ],
+    "education": [
+        ["2014-2019", "BITI MEPHI"],
+        ["2021-2022", "ItGid.info: HTML, CSS, JS, React JS"],
+    ],
+    "achievements": [
+        "Ответственен и инициативен, сохраняю спокойствие в экстренных ситуациях и оперативно нахожу пути разрешения проблемных вопросов",
+        "На данный момент продолжаю активно учиться и получаю знания по Vue JS и Node JS."
+    ],
+    "profile": "Знание технологий и работы с: HTML, CSS, Flex, Grid, Bootstrap, Gulp, HTML5, avaScript, Циклы, Массивы, DOM, Object, Touch, API, AJAX, Class, Object, Prototype, Super, Extends, Get Set, This, Call, Bind, JS Function, Arrow function, Callback function, This, Pure function, Recursion, Closure. Недавно закончил курс по React JS. На данный момент прохожу стажировку в LastBackend"
+};
